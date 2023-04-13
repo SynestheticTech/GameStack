0915e9dbcba3d3d44a80fa8852f7deaf0c474189
Cameron Romo <cromo1225@gmail.com>
2023-04-13 11:51:14 -0600

    Get optimized images

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -34,7 +34,11 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {_.map(platforms, (platform) => (
-        <Icon as={ICON_MAP[platform.slug]} color="gray.500" />
+        <Icon
+          key={platform.slug}
+          as={ICON_MAP[platform.slug]}
+          color="gray.500"
+        />
       ))}
     </HStack>
   );

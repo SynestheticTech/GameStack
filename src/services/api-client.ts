c0f3aa9ab43e24d23207354bc5fdc27696d1da6c
Cameron Romo <cromo1225@gmail.com>
2023-05-12 13:10:50 -0600

    Access api key with vercel

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const RAWG_API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+const RAWG_API_KEY = process.env.VITE_RAWG_API_KEY;
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",

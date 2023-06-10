bbab9328b3bb949a5ecfb5a7507c0588bf2d7634
Cameron Romo <cromo1225@gmail.com>
2023-06-09 18:00:12 -0600

    Test

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const RAWG_API_KEY = process.env.VITE_RAWG_API_KEY;
 
+console.log("RAWG_API_KEY :>> ", RAWG_API_KEY);
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {

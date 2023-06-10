8c1fc3e3d4aba705186ad4e36eab0496288c5797
Cameron Romo <cromo1225@gmail.com>
2023-06-09 18:22:17 -0600

    Remove console.log

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const RAWG_API_KEY = import.meta.env.VITE_RAWG_API_KEY;
 
-console.log("RAWG_API_KEY :>> ", RAWG_API_KEY);
-
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
